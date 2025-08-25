4a662c7651cfffbdb3bfa88032fa76a60effc1ba
Braian-de-Liz <delizbraian@gmail.com>
2025-08-25 19:00:22 -0300

    básico com express

---
+++ express/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Bom Dia meu amigo");
+});
+
+app.get("/sobre", (req, res) => {
+    res.send("Minha pagina sobre");
+});
+
+app.get("/blog", (req, res) => {
+    res.send("Bem-vindo ao meu blog");
+});
+
+app.listen(8082, () => {
+    console.log("bom dia");
+});
