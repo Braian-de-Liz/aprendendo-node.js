767e2058d5a96c60a42d5ffa0e07e93fa895a394
Braian-de-Liz <delizbraian@gmail.com>
2025-08-31 17:27:30 -0300

    selecionar linhas em databases

--- integração_com_mysql/teste.sql
+++ integração_com_mysql/teste.sql
@@ -7,3 +7,12 @@ CREATE TABLE usuarios(
     email VARCHAR(100),
     idade INT
 );
+
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Elias Tino",
+    "emailElias@gmail.com",
+    18
+);
+
+SELECT * FROM usuarios WHERE idade = 22;
