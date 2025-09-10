dec31d770e27a9ba1515e9d2d64b0f2b29ab7f32
Braian-de-Liz <delizbraian@gmail.com>
2025-09-09 22:43:54 -0300

    sequelize testado em uso básico

---
+++ integração_com_mysql/teste_sequelize.js
@@ -0,0 +1,39 @@
+import { Sequelize } from "sequelize";
+const sequelize = new Sequelize("teste", 'root', "Braian1107", {
+    host: "localhost",
+    dialect: 'mysql'
+});
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+});
+
+Postagem.sync({ force: true });
+
+const Usuarios = sequelize.define("usuarios", {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+Usuarios.sync({ force: true });
+
+/* sequelize.authenticate().then(() => {
+    console.log("Conectado");
+}).catch((erro) => {
+    console.log("erro de conexão :" + erro);
+}) */
